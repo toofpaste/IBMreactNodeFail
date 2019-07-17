bb3242df8a1e448b82e6170e1b4e947a4ab7f6a5
Student <example@example.com>
2019-07-17 16:45:26 -0700

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import GetUser from './GetUser';
 const personality = require('watson-personality')
 const PersonalityInsightsV3 = require('ibm-watson/personality-insights/v3');
 
+const personalityInsights = new PersonalityInsightsV3({
+  version: '2017-10-13',
+  username: '{username}',
+  password: '{password}',
+  url: '{url}'
+});
 
 personality('@billyzacsmith')
   .then(function(personalityProfile) {
