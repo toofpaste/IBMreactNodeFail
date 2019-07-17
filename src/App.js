3b09f5f8e2871caea17e65e40aa225359dd21516
Student <example@example.com>
2019-07-17 16:33:11 -0700

    nothing is happening just some api info

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GetUser from './GetUser';
+const personality = require('watson-personality')
 
+personality('@billyzacsmith')
+  .then(function(personalityProfile) {
+    console.log(personalityProfile.traits)
+    console.log(personalityProfile.needs)
+  })
+  .catch(function(error) {
+    console.log(error)
+  })
+
+  
 class App extends Component{
   render(){
   return (
